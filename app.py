c19499aba802b729656e9555dec18e16fc26faac
Damian Wójcicki <wojcicki.d@gmail.com>
2025-04-08 21:56:46 +0200

    Initial commit with parkrun Streamlit app

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+
+
+last_event = st.Page("last_event.py", title="Ostatnie spotkanie", icon="🗓️", default=True)
+best_time = st.Page("best_time.py", title="Najlepsze czasy", icon="👟" )
+last_month = st.Page("last_month.py", title="Ostatni miesiąc", icon="📅" )
+challange_1111 = st.Page("challange_1111.py", title="Wyzwanie 1111", icon="🕒" )
+challange_fib = st.Page("challange_fib.py", title="Wyzwanie fib", icon="🕒" )
+challange_palindrome = st.Page("challange_palindrome.py", title="Wyzwanie palindrom", icon="🕒" )
+
+pg = st.navigation(
+    {
+        "Twój wybór": [last_event, 
+                       best_time
+                       ],
+        "Wyzwania": [challange_1111,
+                    challange_fib,
+                    challange_palindrome ]
+    }
+)
+
+pg.run()
---
+++ challange_fib.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+@st.cache_data
+def get_data(url):
+    try:
+        df = pd.read_csv(url)
+        return df
+    except Exception as e:
+        st.error(f"Błąd połączenia z spreadsheets")
+        return None
+
+st.title("Strona ostatnie spotkanie!")
+
+sheet_url = "https://docs.google.com/spreadsheets/d/1JSTgwT8d623FTplInB7ByzFzoABZc6iCllKzS4oUais/gviz/tq?tqx=out:csv&gid=1425276992"
+df = get_data(sheet_url)
+
+st.write("PB z ostatniej edycji:")
+heigh = min(915,38*len(df))
+st.dataframe(df, height=heigh, use_container_width=False, hide_index= True)
+
+from datetime import datetime
+
+# Aktualna godzina
+now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+with st.sidebar:
+    st.markdown(f"🕒 **Aktualizacja:** {now}", unsafe_allow_html=True)
+    st.markdown("🏃 parkrun Ogród Saski, Lublin")
